dd33bef14c10d14985add25e585cd2b358fc6641
segur <segur.opus@gmail.com>
2019-06-26 21:55:19 +0900

    Add weight chart

---
+++ src/components/LineChart.vue
@@ -0,0 +1,59 @@
+<script>
+import { Line } from 'vue-chartjs';
+
+export default {
+  extends: Line,
+  computed: {
+    datacollection: () => {
+      const labels = ['2015', '2016', '2017', '2018'];
+      const data = [60, 55, 65, 55];
+
+      return {
+        labels,
+        datasets: [
+          {
+            label: 'Weight',
+            pointBackgroundColor: 'white',
+            borderWidth: 1,
+            pointBorderColor: '#249EBF',
+            data,
+          },
+        ],
+      };
+    },
+  },
+  data() {
+    return {
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+              gridLines: {
+                display: true,
+              },
+            },
+          ],
+          xAxes: [
+            {
+              gridLines: {
+                display: false,
+              },
+            },
+          ],
+        },
+        legend: {
+          display: false,
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    };
+  },
+  mounted() {
+    this.renderChart(this.datacollection, this.options);
+  },
+};
+</script>
