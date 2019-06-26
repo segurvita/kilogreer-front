ff30ca9c466f86b0d92669eff21dcc1ff434c55d
segur <segur.opus@gmail.com>
2019-06-26 17:44:24 +0900

    Add Dashboard

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <h1>{{ msg }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
