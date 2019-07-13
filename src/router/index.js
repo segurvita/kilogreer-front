eea5958b86c7fe4a70813181d6badd17cb9e7d4d
segur <segur.opus@gmail.com>
2019-07-13 20:10:16 +0900

    Add comment

--- src/router/index.js
+++ src/router/index.js
@@ -4,16 +4,28 @@ import Dashboard from '@/components/Dashboard';
 
 Vue.use(Router);
 
+/**
+ * 認証済みか検査するナビゲーションガード
+ * @param {*} to 次にナビゲーションされるルート
+ * @param {*} from ナビゲーションされる前のルート
+ * @param {*} next ナビゲーションの制御
+ */
 const requireAuth = (to, from, next) => {
+  // クエリcodeの有無
   if (to.query.code) {
+    // 次のナビゲーションへ遷移
     next();
   } else {
+    // 別のナビゲーションへ遷移
     next({
       path: '/login',
     });
   }
 };
 
+/**
+ * ルーティング
+ */
 export default new Router({
   mode: 'history',
   routes: [
@@ -30,6 +42,7 @@ export default new Router({
     {
       path: '/login',
       beforeEnter(to, from, next) {
+        // ログインページへ遷移
         window.location = process.env.VUE_APP_API_LOGIN;
         next();
       },
