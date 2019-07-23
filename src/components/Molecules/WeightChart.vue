d2342b90d9b9433dd6f2028e5567ad8c32c6fb66
segur <segur.opus@gmail.com>
2019-07-23 23:01:25 +0900

    Show date in M/D format

--- src/components/Molecules/WeightChart.vue
+++ src/components/Molecules/WeightChart.vue
@@ -10,6 +10,7 @@
 </template>
 
 <script>
+import dayjs from 'dayjs';
 import { mapGetters, mapActions } from 'vuex';
 import LineChart from '@/components/Atoms/LineChart';
 
@@ -21,7 +22,8 @@ export default {
   computed: {
     ...mapGetters('weights', ['dailyList', 'loading']),
     times() {
-      return this.dailyList.map(item => item.createdDate);
+      return this.dailyList.map(item =>
+        dayjs(item.createdDate).format('M/D'));
     },
     weights() {
       return this.dailyList.map(item =>
