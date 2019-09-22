e7505c6dea07bef5703d901f00309739dab17310
segur <segur.opus@gmail.com>
2019-09-22 14:46:43 +0900

    Removed function to add empty data

--- src/store/modules/weights.js
+++ src/store/modules/weights.js
@@ -68,9 +68,6 @@ export default {
       // 日付単位のリストを作成開始
       const dailyList = [list[0]]; // 初期値は先頭値
       list.reduce((prev, current) => {
-        // 今注目している日付
-        const currentDate = moment(current.createdDate);
-
         if (prev.createdDate === current.createdDate) {
           // 前の測定と日付が同じなら、最小値を更新
           dailyList[dailyList.length - 1].weightValue = Math.min(
@@ -78,19 +75,6 @@ export default {
             current.weightValue,
           );
         } else {
-          // 前の測定との差を計算
-          const prevDate = moment(prev.createdDate);
-          const diffDay = currentDate.diff(prevDate, 'days');
-
-          // 測定値がない日付のデータを追加
-          for (let i = 0; i < diffDay - 1; i += 1) {
-            const iteDate = prevDate.add(1, 'd').format('YYYY/MM/DD');
-            dailyList.push({
-              createdDate: iteDate,
-              weightValue: current.weightValue,
-            });
-          }
-
           // dailyListに追加
           dailyList.push({
             createdDate: current.createdDate,
